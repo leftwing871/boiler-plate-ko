fed054b8945e6aea280fd28894563911da8f5c75
oscar <leftwing871@gmail.com>
2020-09-08 07:56:15 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI: 'mongodb+srv://master:Master1!@boilerplate.wulgc.mongodb.net/test?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -3,12 +3,35 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 5000
 
-mongoose.connect('mongodb+srv://master:Master1!@boilerplate.wulgc.mongodb.net/test?retryWrites=true&w=majority', {
+const { User } = require("./models/User")
+const bodyParser = require("body-parser")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+const config = require('./config/key')
+
+//application/json
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!!~'))
+
+
+app.post('/register', (req, res) => {
+    
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`example app listening on port ${port}`))
 
